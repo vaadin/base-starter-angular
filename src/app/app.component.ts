6ca484138b373cbf27b8c8495ddc1224011b9e52
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2018-04-05 11:30:36 +0300

    Work with p3 versions of vaadin-text-field and vaadin-button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import '@vaadin/vaadin-button/vaadin-button.js';
+import '@vaadin/vaadin-text-field/vaadin-text-field.js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
