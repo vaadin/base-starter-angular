b4cddccc6911c85b7e47b95e3748d3a8ca6c0826
Tomi Virkki <virkki@vaadin.com>
2019-07-19 12:38:18 +0300

    wip ComponentPortal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, Injector, ApplicationRef } from '@angular/core';
 import '@vaadin/vaadin-button/vaadin-button.js';
 import '@vaadin/vaadin-text-field/vaadin-text-field.js';
+import '@vaadin/vaadin-dialog/vaadin-dialog.js';
+import { OverlayComponent } from './overlay.component';
+
+import { DomPortalHost, Portal, ComponentPortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,38 @@ import '@vaadin/vaadin-text-field/vaadin-text-field.js';
 })
 export class AppComponent {
   title = 'hello-angular';
+
+  private portalHost: DomPortalHost;
+
+  private portal: ComponentPortal<OverlayComponent>;
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector,
+    private applicationRef: ApplicationRef,
+   ) {
+    this.renderer = this.renderer.bind(this);
+   }
+
+   private foo = document.createElement('div');
+
+   renderer(root: HTMLElement) {
+    root.appendChild(this.foo);
+   }
+
+   ngOnInit() {
+    // Create a portalHost from a DOM element
+    this.portalHost = new DomPortalHost(
+      this.foo,
+      this.componentFactoryResolver,
+      this.applicationRef,
+      this.injector
+    );
+
+    // Locate the component factory for the OverlayComponent
+    this.portal = new ComponentPortal(OverlayComponent);
+
+    // Attach portal to host
+    this.portalHost.attach(this.portal);
+  }
 }
--- src/app/overlay.module.ts
+++ src/app/overlay.module.ts
@@ -1,20 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { OverlayComponent } from './overlay.component';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
-    AppComponent,
     OverlayComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: [OverlayComponent]
+  bootstrap: [OverlayComponent],
+  exports: [ OverlayComponent ]
 })
-export class AppModule { }
+export class OverlayModule { }
