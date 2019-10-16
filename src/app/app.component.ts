df971f50aca5cbb6519915408d31f79933334aff
Tomi Virkki <virkki@vaadin.com>
2019-10-16 11:38:34 +0300

    Added a few more examples
    Apparently some of the released modules still have @extends annotations. Needed to add a script to package.json that removes @extends annotations to avoid a transpilation problems.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, ComponentFactoryResolver, Injector, ApplicationRef, ViewChil
 import '@vaadin/vaadin-button/vaadin-button.js';
 import '@vaadin/vaadin-text-field/vaadin-text-field.js';
 import '@vaadin/vaadin-dialog/vaadin-dialog.js';
+import '@vaadin/vaadin-grid/vaadin-grid.js';
+import '@vaadin/vaadin-grid/vaadin-grid-tree-column.js';
+import '@vaadin/vaadin-menu-bar/vaadin-menu-bar.js';
+import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import { DomPortalHost, TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
@@ -23,6 +27,32 @@ export class AppComponent {
 
   renderer = (root: HTMLElement) => root.appendChild(this.overlayHost);
 
+  constructor() {
+    // Only register styles once!
+    registerStyles('vaadin-grid-tree-toggle', css`
+      :host {
+        color: blue;
+      }
+
+      [part~=toggle] {
+        color: red;
+      }
+    `);
+
+    registerStyles('vaadin-button', css`
+      :host(.blue) [part~=label] {
+        color: blue;
+      }
+    `);
+
+    registerStyles('vaadin-menu-bar-button', css`
+      [part~=label] {
+        color: red;
+      }
+    `);
+
+  }
+
   ngOnInit() {
     new DomPortalHost(
       this.overlayHost,
@@ -31,4 +61,57 @@ export class AppComponent {
       this.injector
     ).attach(this.testTemplatePortal);
   }
+
+  dataProvider = (params, cb) => {
+    const level = params.parentItem ? params.parentItem.level + 1 : 0;
+    cb(this.generator(level, params.parentItem));
+  };
+
+  nameRenderer = (root: HTMLElement, column, {item}) => {
+    if (!root.firstElementChild) {
+      root.appendChild(document.createElement('strong'));
+    }
+    root.firstElementChild.textContent = 'name-' + item.foo;
+  }
+
+  generator = (level, parent={key: '0'}) =>
+    Array.from(new Array(5))
+      .map((i, index) => {
+        return {
+          key: `${parent.key}-${index}`,
+          foo: 'bar',
+          level,
+          hasChildren: level < 2
+        };
+      });
+
+  menuItems = [
+    {
+      text: 'Project',
+      children: [
+        {text: 'Users',
+          children: [{text: 'List'}, {text: 'Add'}]
+        },
+        {text: 'Billing',
+          children: [{text: 'Invoices'}, {text: 'Balance Events'}]
+        },
+      ]
+    },
+    {
+      text: 'Account',
+      children: [
+        {text: 'Edit Profile'},
+        {text: 'Privacy Settings'}
+      ]
+    },
+    {text: 'Sign Out'}
+  ];
+
+  menuBarItemSelected = e => {
+    console.log(e.detail.value.text + ' selected');
+  }
+
+  // Empty function to override the menu-bar's _onMouseOver
+  // monkey patching a private member isn't recommended!
+  onMouseOver = () => {}
 }
